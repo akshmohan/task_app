e0efbce3f5a111ad4b761d416f612bafd0fa389b
akshmohan <145959795+akshmohan@users.noreply.github.com>
2024-12-09 17:41:51 +0530

    Finished designing the SignUpPage

--- lib/features/auth/pages/signup_page.dart
+++ lib/features/auth/pages/signup_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: implementation_imports, unnecessary_import, unused_import, prefer_const_constructors
+// ignore_for_file: implementation_imports, unnecessary_import, unused_import, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -10,12 +10,113 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  void signupUser() {
+    if (formKey.currentState!.validate()) {}
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Signup Page'),
+        body: Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Form(
+        key: formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Sign Up.",
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextFormField(
+              controller: nameController,
+              decoration: InputDecoration(
+                hintText: "Name",
+              ),
+              validator: (value) {
+                if (value == null || value.trim().isEmpty) {
+                  return "Name field cannot be empty!";
+                } else {
+                  return null;
+                }
+              },
+            ),
+            const SizedBox(height: 15),
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(
+                hintText: "Email",
+              ),
+              validator: (value) {
+                if (value == null ||
+                    value.trim().isEmpty ||
+                    value.contains("@")) {
+                  return "Email field cannot be empty!";
+                } else {
+                  return null;
+                }
+              },
+            ),
+            const SizedBox(height: 15),
+            TextFormField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                hintText: "Password",
+              ),
+              validator: (value) {
+                if (value == null ||
+                    value.trim().isEmpty ||
+                    value.contains("@")) {
+                  return "Password field cannot be empty!";
+                } else {
+                  return null;
+                }
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: signupUser,
+              child: Text(
+                "SIGN UP",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ),
+            SizedBox(height: 20),
+            RichText(
+              text: TextSpan(
+                text: "Already have an account? ",
+                style: Theme.of(context).textTheme.titleMedium,
+                children: [
+                  TextSpan(
+                    text: "Sign In",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:frontend/features/auth/pages/signup_page.dart';
 
@@ -14,10 +16,44 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Task App',
       theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+            contentPadding: const EdgeInsets.all(27),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.grey.shade300,
+                width: 3,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                width: 3,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(
+                width: 3,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.red,
+                width: 3,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            )),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                minimumSize: const Size(double.infinity, 60),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ))),
         primarySwatch: Colors.blue,
       ),
       home: const SignupPage(),
     );
   }
 }
-
