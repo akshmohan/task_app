ac999832854a876929305ca4dabda2666cd51fdd
akshmohan <145959795+akshmohan@users.noreply.github.com>
2024-12-09 15:06:46 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/features/auth/pages/signup_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Task App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SignupPage(),
+    );
+  }
+}
+
